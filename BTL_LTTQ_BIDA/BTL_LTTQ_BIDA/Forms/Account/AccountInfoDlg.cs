ab5bf36996cfc08dc633523cd54e2fca9416f5c5
trinhvan205 <tranvantrinh1301@gmail.com>
2025-10-28 15:02:01 +0700

    new .gitignore and BTL_LTTQ_BIDA

---
+++ BTL_LTTQ_BIDA/BTL_LTTQ_BIDA/Forms/Account/AccountInfoDlg.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTL_LTTQ_BIDA.Forms.Account
+{
+    public partial class AccountInfoDlg : Form
+    {
+        public AccountInfoDlg()
+        {
+            InitializeComponent();
+        }
+    }
+}
